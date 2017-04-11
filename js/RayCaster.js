03e1695f5f9c16d01a7fbffe843d7b7be83bd770
Mariusz <mariusz.baran23@gmail.com>
2017-04-12 00:08:58 +0200

    Znajdowanie meshy pod
    Nie testowałem xd

---
+++ js/RayCaster.js
@@ -0,0 +1,52 @@
+// Returns all elements that are under object on y axis
+// @param axis(0 - x, other - z) specifies from which axis camera is looking at object (default is x)
+
+// Po polacku cebulacku
+// Funkcja zwraca wszystkie meshe(które z założenia są BoxGeometry), które znajdują się pod podanym @object(niżej w osi y)
+// w zależności od punktu patrzenia, ustalony w @axis
+function GetElementsUnder(scene, object, axis)
+{
+	var ElementsUnder = [];
+
+	var object_data = {
+		x: object.position.x,
+		y: object.position.y,
+		z: object.position.z,
+		w: object.geometry.parameters.width,
+		h: object.geometry.parameters.depth
+	};
+
+	// Get all elements from scene
+	for(var i = 0; i < scene.children.length; ++i)
+	{
+		var element = scene.children[i];
+
+		var element_data = {
+			x: element.position.x,
+			y: element.position.y,
+			z: element.position.z,
+			w: element.geometry.parameters.width,
+			h: element.geometry.parameters.depth
+		};
+
+		if(element_data.y >= object_data.y) // Element is not under @object - skip this iteration
+			continue;
+
+		if(axis == 0) // x
+		{
+			if((object_data.x > element_data.x - (element_data.w / 2) - (object_data.w / 2)) && (object_data.x < element_data.x + (element_data.w / 2) + (object_data.w / 2)))
+			{
+				ElementsUnder.push(element);
+			}
+		}
+		else // z
+		{
+			if((object_data.z > element_data.z - (element_data.w / 2) - (object_data.w / 2)) && (object_data.z < element_data.z + (element_data.w / 2) + (object_data.w / 2)))
+			{
+				ElementsUnder.push(element);
+			}
+		}
+	}
+
+	return ElementsUnder;
+}
